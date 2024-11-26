cd2e49979ced438544a7d778a7216935a42d8fa6
Astyell <etoiledeplante@gmail.com>
2024-11-26 10:00:16 +0100

    V1.1.0 Accueil.php + V1.1.0 commun.css

--- app/Views/Accueil.php
+++ app/Views/Accueil.php
@@ -1,1 +1,21 @@
-	<div class="bg"></div>
+<!--
+@author   : Sarah Hautot, Alizéa Lebaron
+@since    : 25/11/2024
+@version  : 1.1.0 - 26/11/2024
+-->
+
+<div class="bg">
+	<div class="vh-100 d-flex justify-content-center align-items-center">
+		<div class="infoaccueil border text-align-center m-5 p-5 w-50  rounded">
+
+			<div class="d-flex flex-column align-items-center">
+				<h2 class="mb-5 fs-1"> Bienvenue sur Quicket ! </h2>
+
+				<p class="mt-3 fs-4">Quicket est un logiciel de gestion de tâche.</p>
+				<p class="mt-3 fs-4">Pour accéder à vos tâches, n'hésitez pas à vous <a href="/connexion" class="text-success">connecter</a> !</p>
+				<p class="fs-4" >Vous êtes nouveau par ici ? Vous pouvez vous inscrire en <a href="/inscription" class="text-success">cliquant ici</a> !</p>
+			</div>
+
+		</div>
+	</div>
+</div>
