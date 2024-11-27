1f0ae6a200b25e2fb5e6cb1e4ae8f0e896e4ade6
Ashanti Njanja <shantil_ytb@outlook.com>
2024-11-27 09:55:11 +0100

    Filtre après touche Entrée

--- public/assets/js/redirection_filtre.js
+++ public/assets/js/redirection_filtre.js
@@ -1,9 +1,18 @@
+const titreInput = document.getElementById('titre-recherche')
+titreInput.addEventListener('keyup', 
+    (event) =>
+    {
+        if (event.key === 'Enter')
+            redirection_recherche()   
+    }
+);
+
 function redirection_recherche()
 {
 	
 	trierPar = document.getElementById('trier-par').value;	
 	ordre    = document.getElementById('ordre').value;	
-	titre    = document.getElementById('titre-recherche').value;
+	titre    = titreInput.value;
 
 	if (!titre || titre.trim().length === 0)
 		location.replace("?trierPar=" + trierPar + "&ordre=" + ordre);
