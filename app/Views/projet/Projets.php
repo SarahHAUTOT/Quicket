cdc0da71cbc91b6b981cfae7f7d1f15ea56872c0
alizealebaron <alizea.lebaron2@gmail.com>
2024-11-29 16:26:58 +0100

    Fot dortogrf

--- app/Views/projet/Projets.php
+++ app/Views/projet/Projets.php
@@ -148,7 +148,7 @@
 		<div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
 			<div class="modal-content">
 				<div class="modal-header">
-					<h5 class="modal-title" id="exampleModalLabel">Ajouter une tache</h5>
+					<h5 class="modal-title" id="exampleModalLabel">Ajouter un nouveau projet</h5>
 					<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
 					</button>
 				</div>
